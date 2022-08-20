11da5f786488db2cceb104ff107d96b658865a69
taiga <b21120429@njupt.edu.cn>
2022-08-20 20:12:36 +0800

    refactor: change log format

--- config.go
+++ config.go
@@ -1,6 +1,8 @@
 package pixiv
 
 import (
+	"bytes"
+	"fmt"
 	"net/http"
 	"net/url"
 	"os"
@@ -18,11 +20,29 @@ var (
 	myLog  = logrus.New()
 )
 
+type myFormatter struct {}
+
 func init() {
 	myLog.SetOutput(os.Stdout)
 	myLog.SetLevel(logrus.DebugLevel)
+	myLog.SetFormatter(&myFormatter{})
 	readConfig()
 	proxyConfig()
+}
+
+func (f *myFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	var b *bytes.Buffer
+	if entry.Buffer != nil {
+		b = entry.Buffer
+	} else {
+		b = &bytes.Buffer{}
+	}
+	
+	timestamp := entry.Time.Format("2006-01-02 15:04:05")
+	newLog := fmt.Sprintf("[%s] [%s] %s\n", timestamp, entry.Level, entry.Message)
+
+	b.WriteString(newLog)
+	return b.Bytes(), nil
 }
 
 func readConfig() {
