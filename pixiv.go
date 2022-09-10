73544b1ecf3dc8be785f93bd49b81e5fefef57f7
yfaimisaka <b21120429@njupt.edu.cn>
2022-09-10 11:39:52 +0800

    fix: some fix

--- pixiv.go
+++ pixiv.go
@@ -38,28 +38,28 @@ func (p *pixiv) getImgUrls(ids chan string) chan string {
 			URL := fmt.Sprintf("https://www.pixiv.net/artworks/%s", id)
 			req, err := http.NewRequest("GET", URL, nil)
 			if err != nil {
-				p.log.WithError(err).Fatalf("Fail to create request, URL=%s", URL)
+				p.log.WithError(err).Fatalf("pixiv: Fail to create request, URL=%s", URL)
 			}
 
 			res, err := client.Do(req)
 			if err != nil {
-				p.log.WithError(err).Fatalln("Fail to get response")
+				p.log.WithError(err).Fatalln("pixiv: Fail to get response")
 			}
 			if res.StatusCode != 200 {
-				p.log.Fatalf("Response Code=%d", res.StatusCode)
+				p.log.Fatalf("pixiv: Response Code=%d", res.StatusCode)
 			}
 			defer res.Body.Close()
 
 			htmlByte, err := io.ReadAll(res.Body)
 			if err != nil {
-				p.log.WithError(err).Fatalln("Fail to read response")
+				p.log.WithError(err).Fatalln("pixiv: Fail to read response")
 			}
 			html := string(htmlByte)
 
 			reg := regexp.MustCompile(`(?s)"original":"(.*?)"}`)
 			u := reg.FindStringSubmatch(html)[1]
 			if u == "" {
-				p.log.WithError(err).Fatalf("Fail to get url of id=%s", id)
+				p.log.WithError(err).Fatalf("pixiv: Fail to get url of id=%s", id)
 			}
 
 			p.log.Infof("Got work that id is %s", id)
@@ -90,13 +90,13 @@ func (p *pixiv) upLoadImg(imgUrls chan string) {
 		go func(url string) {
 			req, err := http.NewRequest("GET", url, nil)
 			if err != nil {
-				p.log.WithError(err).Fatalf("Fail to send request, URL=%s", url)
+				p.log.WithError(err).Fatalf("pixiv: Fail to send request, URL=%s", url)
 			}
 			setHeader(req)
 
 			res, _ := client.Do(req)
 			if res.StatusCode != 200 {
-				p.log.Fatalf("URL Code=%d", res.StatusCode)
+				p.log.Fatalf("pixiv: URL Code=%d", res.StatusCode)
 			}
 			defer res.Body.Close()
 
@@ -104,7 +104,7 @@ func (p *pixiv) upLoadImg(imgUrls chan string) {
 			contentType := res.Header.Get("content-type")
 			n, err := minioClient.PutObject(bucketName, name, res.Body, res.ContentLength, minio.PutObjectOptions{ContentType: contentType})
 			if n == 0 || err != nil {
-				p.log.WithError(err).Fatalln("Fail to upload to minio")
+				p.log.WithError(err).Fatalln("pixiv: Fail to upload to minio")
 			}
 			p.log.Infoln(name + "upload succeded")
 			mu.Lock()
@@ -133,7 +133,7 @@ func (p *pixiv) downLoadImg(imgUrls chan string) {
 	if ok, _ := pathExists(p.savePath + sep + p.fileDir); !ok {
 		err := os.Mkdir(p.savePath + sep + p.fileDir, 0766)
 		if err != nil {
-			p.log.WithError(err).Fatalf("Fail to create dir: %s", p.savePath+p.fileDir)
+			p.log.WithError(err).Fatalf("pixiv: Fail to create dir: %s", p.savePath+p.fileDir)
 		}
 	}
 	for imgUrl := range imgUrls {
@@ -143,19 +143,19 @@ func (p *pixiv) downLoadImg(imgUrls chan string) {
 			defer wg.Done()
 			req, err := http.NewRequest("GET", url, nil)
 			if err != nil {
-				p.log.WithError(err).Fatalf("Fail to create request, URL=%s", url)
+				p.log.WithError(err).Fatalf("pixiv: pixiv: Fail to create request, URL=%s", url)
 			}
 			setHeader(req)
 
 			res, _ := client.Do(req)
 			if res.StatusCode != 200 {
-				p.log.Fatalf("Response Code=%d", res.StatusCode)
+				p.log.Fatalf("pixiv: pixiv: Response Code=%d", res.StatusCode)
 			}
 			defer res.Body.Close()
 
 			imgByte, err := io.ReadAll(res.Body)
 			if err != nil {
-				p.log.WithError(err).Fatalln("Fail to read response")
+				p.log.WithError(err).Fatalln("pixiv: Fail to read response")
 			}
 
 			
@@ -163,15 +163,15 @@ func (p *pixiv) downLoadImg(imgUrls chan string) {
 			if ok, _ := pathExists(fileName); !ok {
 				file, err := os.Create(fileName)
 				if err != nil {
-					p.log.WithError(err).Fatalf("Fail to create file: %s", fileName)
+					p.log.WithError(err).Fatalf("pixiv: Fail to create file: %s", fileName)
 				}
 
 				writer := bufio.NewWriter(file)
 				nn, err := writer.Write(imgByte)
 				if nn == 0 || err != nil {
-					p.log.WithError(err).Fatalf("Fail to write picture: %s", fileName)
+					p.log.WithError(err).Fatalf("pixiv: Fail to write picture: %s", fileName)
 				}
-				p.log.Infoln(fileName + " save successfully")
+				p.log.Infoln("pixiv " + fileName + " save successfully")
 
 			}
 			mu.Lock()
@@ -179,6 +179,6 @@ func (p *pixiv) downLoadImg(imgUrls chan string) {
 			mu.Unlock()
 		}(imgUrl, count)
 		wg.Wait()
-		p.log.Infof("Total: %d", count)
+		p.log.Infof("pixiv: Total: %d", count)
 	}
 }
